0766f589c9af08e688a37867274a415077f1d85b
Douglas Schneider <ds3@ualberta.ca>
2014-04-17 16:04:55 -0600

    remove the old simple cli implementation

--- pymacco/client/simple.py
+++
@@ -1,8 +0,0 @@
-from pymacco.ui.simpleCli import SimpleCli
-from pymacco.ui.base import start
-
-if __name__ == "__main__":
-    try:
-        start(SimpleCli)
-    except KeyboardInterrupt:
-        pass
